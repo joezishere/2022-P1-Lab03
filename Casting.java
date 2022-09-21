cea3247cc0e3a03dd0143cf347b0575b5a86d9f6
Joezishere <Joseph.zinni@vtsd.con>
2022-09-21 08:30:47 -0400

    Initial File Upload

---
+++ Casting.java
@@ -0,0 +1,17 @@
+public class Casting {
+  public static void main(String args[]){
+      int dogs =(int) 4.5;
+     double exactDogs = 7.2;
+
+     System.out.println(dogs);
+      System.out.println(exactDogs);
+      char joesFave ='S';
+      System.out.println((int) joesFave);
+      joesFave++;
+      System.out.println(joesFave);
+      boolean sadness = true;
+      System.out.println(sadness);
+  }
+
+
+}
